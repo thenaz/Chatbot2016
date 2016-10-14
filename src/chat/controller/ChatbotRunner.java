e2564786c523e9458a8dbcb15aeb8b6fe7b75728
thenaz <anaz7963@740s-pl-126.local>
2016-10-14 09:51:35 -0600

    Added methods that were missing
    9/14

--- src/chat/controller/ChatbotRunner.java
+++ src/chat/controller/ChatbotRunner.java
@@ -3,4 +3,9 @@ package chat.controller;
 public class ChatbotRunner
 {
 
+	public static void main (String [] args)
+	{
+		ChatbotController chatAppController = new ChatbotController();
+		chatAppController.start();
+	}
 }
