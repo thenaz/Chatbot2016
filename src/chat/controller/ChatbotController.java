c5b703bd2c7407fc3992f5964773388c601113c8
thenaz <anaz7963@740s-pl-126.local>
2016-10-18 08:55:18 -0600

    Added a question

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,10 +1,28 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import chat.view.ChatViewer;
+
 public class ChatbotController
 {
+	private Chatbot stupidBot;
+	private ChatViewer display;
+	
+	public ChatbotController()
+	{
+		stupidBot = new Chatbot("unintelligent chat machine");
+		display = new ChatViewer();
+	}
+	
 
 	public void start()
 	{
+		String response = "words";
 		
+		while(stupidBot.lengthChecker(response))
+		{
+			
+			response = display.collectResponse("What's good JB?");
+		}
 	}
 }
